2dccd5355899b017b940d31ccc77ee070fb58476
monoharada <harada@monosus.co.jp>
2022-10-16 23:01:12 +0900

    chore: update view file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,21 @@ import "@unocss/reset/tailwind.css";
 import "uno.css";
 import "@/styles/root.css";
 
+import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import type { ReactElement, ReactNode } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
 }
-
-export default MyApp;
