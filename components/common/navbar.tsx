c5a6eee6e209a13399f792ffeb8bbe78ee179ef8
monoharada <harada@monosus.co.jp>
2022-11-06 11:39:43 +0900

    test: lint-stage test 4

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -1,21 +1,34 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { memo } from "react";
 
+const navigation = [
+  {
+    href: "/",
+    page: "Home",
+  },
+  {
+    href: "/blog",
+    page: "Blog",
+  },
+];
 function Navbar() {
+  const router = useRouter();
+  const style = (href: string) => (router.asPath === href ? current : other);
+
   return (
     <nav className="bg-teal-700 px-10">
       <div className="relative h-16 flex items-center justify-between">
         <div className="flex space-x-4">
-          <Link href="/">
-            <a className="rounded-md bg-teal-900 px-3 py-2 text-sm font-medium text-white">
-              Home
-            </a>
-          </Link>
-          <Link href="/blog">
-            <a className="rounded-md px-3 py-2 text-sm font-medium text-teal-100 hover:(bg-teal-600 text-white)">
-              Blog
-            </a>
-          </Link>
+          {navigation.map((navItem) => (
+            <Link
+              href={navItem.href}
+              className={`${base} ${style(navItem.href)}`}
+              key={navItem.page}
+            >
+              {navItem.page}
+            </Link>
+          ))}
         </div>
       </div>
     </nav>
@@ -23,3 +36,8 @@ function Navbar() {
 }
 
 export default memo(Navbar);
+
+const base = "rounded-md px-3 py-2 text-sm font-medium";
+
+const current = " bg-teal-900  text-white";
+const other = "rounded-md  text-teal-100 hover:(bg-teal-600 text-white)";
